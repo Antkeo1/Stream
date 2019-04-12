0e05ac85e1cffbf0ef5fbf37ea4e37787fb40e9a
Antkeo1 <sleav143@gmail.com>
2019-04-12 12:55:17 -0400

    created routes to set path to each component

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,4 +1,10 @@
 import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+import StreamCreate from './streams/StreamCreate'
+import StreamList from './streams/StreamList'
+import StreamShow from './streams/StreamShow'
+import StreamEdit from './streams/StreamEdit'
+import StreamDelete from './streams/StreamDelete'
 
 class App extends React.Component {
   state = {
@@ -8,7 +14,15 @@ class App extends React.Component {
   render() {
     return (
       <div className='ui container '>
-
+        <BrowserRouter>
+          <div>
+            <Route path='/' exact component={StreamList}/>
+            <Route path='/streams/new' exact component={StreamCreate}/>
+            <Route path='/streams/edit' exact component={StreamEdit}/>
+            <Route path='/streams/show' exact component={StreamShow}/>
+            <Route path='/streams/delete' exact component={StreamDelete}/>
+          </div>
+        </BrowserRouter>
       </div>
     )
   }
