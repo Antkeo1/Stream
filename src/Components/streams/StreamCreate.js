4c6449c076f896fa5616bde26e66b12ed65d2c56
Antkeo1 <sleav143@gmail.com>
2019-04-20 10:27:03 -0400

    created input form for stream create using redux-form

--- src/Components/streams/StreamCreate.js
+++ src/Components/streams/StreamCreate.js
@@ -1,7 +1,32 @@
 import React from 'react'
+// Field is react component created in redux-form
+// reduxForm is function simular to connect function
+// with it we will be able to call action creators and
+// get form data into our component
+import {Field, reduxForm } from 'redux-form'
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>
+class StreamCreate extends React.Component {
+  renderInput = ({input, label}) => {
+    console.log(label)
+    return (
+      <div className='field'>
+      <label>{label}</label>
+        <input {...input} />
+      </div>
+    )
+  }
+
+  render() {
+    return (
+      <form className='ui form'>
+        <Field name='title' component={this.renderInput} label='Enter title'/>
+        <Field name='desciption' component={this.renderInput} label='Enter desiption' />
+      </form>
+    )
+  }
 }
 
-export default StreamCreate
+
+export default reduxForm({
+  form:'StreamCreate'
+})(StreamCreate)
--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,4 +1,5 @@
 import {combineReducers} from 'redux'
+// renaming reducer, from redux-form, formReducer
 import { reducer as formReducer } from 'redux-form'
 import authReducer from './authReducer'
 
