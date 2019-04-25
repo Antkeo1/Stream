93a9905f9673d97f7fba29d1b8e7662e63567649
Antkeo1 <sleav143@gmail.com>
2019-04-25 10:18:52 -0400

    added initialValues to edit form, a prop from redux form

--- src/Components/streams/StreamEdit.js
+++ src/Components/streams/StreamEdit.js
@@ -1,14 +1,38 @@
 import React from 'react'
 import { connect} from 'react-redux'
-import { editStream } from '../../Actions'
+import { editStream, fetchStream } from '../../Actions'
+import StreamForm from './StreamForm'
 
-const StreamEdit = (props) => {
-  console.log(props)
-  return <div>StreamEdit</div>
+class StreamEdit extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id)
+  }
+
+  onSubmit = (formValues) => {
+    console.log(formValues)
+    this.props.editStream(formValues)
+  }
+
+  render() {
+    if(!this.props.stream) {
+      return <div>Loading...</div>
+    } else {
+      return (
+        <div>
+          <h3>Edit a Stream</h3>
+          <StreamForm
+            initialValues
+            onSubmit={this.onSubmit}/>
+        </div>
+      )
+    }
+  }
 }
 
 const mapStateToProps = (state, ownProps) => {
   return {stream: state.streams[ownProps.match.params.id]}
 }
 
-export default connect(mapStateToProps, { editStream})(StreamEdit)
+export default connect(mapStateToProps, {
+  editStream, fetchStream}
+)(StreamEdit)
---
+++ src/Components/streams/StreamForm.js
@@ -0,0 +1,65 @@
+import React from 'react'
+// Field is react component created in redux-form
+// reduxForm is function simular to connect function
+// with it we will be able to call action creators and
+// get form data into our component
+import {Field, reduxForm } from 'redux-form'
+
+class StreamForm extends React.Component {
+  renderError = ({error, touched}) => {
+    if(touched && error) {
+      return (
+        <div className='ui error message'>
+          <div className='header'>{error}</div>
+        </div>
+      )
+    }
+  }
+
+  renderInput = ({input, label, meta}) => {
+    const className = `field ${meta.error && meta.touched ? 'error': ''}`
+    return (
+      <div className={className}>
+      <label>{label}</label>
+        <input {...input} autoComplete='off'/>
+        {this.renderError(meta)}
+      </div>
+    )
+  }
+
+  // api request
+  onSubmit = (formValues) => {
+    this.props.onSubmit(formValues)
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.props.handleSubmit(this.onSubmit)}
+            className='ui form error'>
+        <Field name='title' component={this.renderInput} label='Enter title'/>
+        <Field name='description' component={this.renderInput} label='Enter description' />
+        <button className='ui button primary'>Submit</button>
+      </form>
+    )
+  }
+}
+
+// to make sure input is filled in and deal with error
+const validate = formValues => {
+  const errors = {}
+  if(!formValues.title) {
+    // only run if user didnt enter title
+    errors.title = 'Please Enter title'
+  }
+
+  if (!formValues.description) {
+    errors.description = 'Please Enter description'
+  }
+
+  return errors
+}
+
+export default reduxForm({
+  form:'StreamForm',
+  validate
+})(StreamForm)
