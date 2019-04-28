7822a65fb5f00335346fec7e7c21482f2eb50414
Antkeo1 <sleav143@gmail.com>
2019-04-28 13:50:24 -0400

    used createPortal() to create a modal for StreamDelete

--- src/Components/streams/StreamDelete.js
+++ src/Components/streams/StreamDelete.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import Modal from '../Modal'
 import { connect } from 'react-redux'
 import { deleteStream} from '../../Actions'
 
 
 const StreamDelete = () => {
-  return <div>StreamDelete</div>
+  return (
+    <div>
+      StreamDelete
+      <Modal />
+    </div>
+  )
 }
 
 export default connect(null, {deleteStream})(StreamDelete)
